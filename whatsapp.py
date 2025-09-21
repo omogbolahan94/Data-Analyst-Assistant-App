5501def69c8e253656d98587219acd22ac817855
omogbolahan94 <gabrielomogbolahan1@gmail.com>
2025-09-21 19:50:16 +0100

    whatapp authentication (not concern AI agent)

---
+++ whatsapp.py
@@ -0,0 +1,7 @@
+import pywhatkit
+
+mobile_n = "+2348181019845"
+
+# pywhatkit.sendwhatmsg(mobile_n, "Testing message from colab.", 8, 57)
+
+pywhatkit.sendwhatmsg_instantly(mobile_n, "Testing message from colab.", 9, 2)
